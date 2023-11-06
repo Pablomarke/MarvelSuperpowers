9fd400b1bd28a508fe458b08e5b7055cfcbee960
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-11-06 01:26:28 +0100

    Cambio a Main

--- MarvelSuperpowers/MarvelSuperpowersApp.swift
+++ MarvelSuperpowers/MarvelSuperpowersApp.swift
@@ -13,7 +13,7 @@ struct MarvelSuperpowersApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
